189d55da9c621c0402c3c3429d47440c04a6464e
SoumitraPaul047 <psoumitra47@gmail.com>
2026-07-29 05:41:14 +0600

    Added database module

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+ $server = "localhost";
+ $user = "SoumitraPaul";
+ $pass = "1234567890";
+ $dbname = "library_management_system";
+ $conn = mysqli_connect('localhost', 'root', '', 'library_management_system');
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+ 
+?>
